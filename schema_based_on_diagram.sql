7e1c4c5adba16fd252b36932975e191c1de1666c
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-10-06 15:46:20 -0500

    add indexes for foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -79,3 +79,9 @@ ALTER TABLE treatments_history
 ADD CONSTRAINT fk_treatments
 FOREIGN KEY (treatment_id)
 REFERENCES treatments (id);
+
+-- Indexes for Foreign Keys
+CREATE INDEX ON medical_histories (patient_id);
+CREATE INDEX ON invoices (medical_history_id);
+CREATE INDEX ON invoice_items (invoice_id, treatment_id);
+CREATE INDEX ON treatments_history (medical_history_id, treatment_id);
